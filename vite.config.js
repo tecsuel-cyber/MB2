61dba3c467bbd4518d647ac980c975665b9397c8
tecsuel-cyber <tec.suel@gmail.com>
2025-09-17 14:15:53 -0300

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: './', // 🔹 isso é essencial para funcionar no Vercel
+})
