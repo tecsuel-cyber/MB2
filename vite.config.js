fac1a3fc98f53de71cbbb068f66140257e2d7d2b
tecsuel-cyber <tec.suel@gmail.com>
2025-09-19 10:46:23 -0300

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Configuração corrigida para deploy no Vercel
-export default defineConfig({
-  plugins: [react()],
-  base: './',
-})
